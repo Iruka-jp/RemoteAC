7260913b846caf887068c3610d1bdde16893f3a2
Iruka-jp <52767185+Iruka-jp@users.noreply.github.com>
2026-02-26 22:44:31 +0900

    feat: assign change page on click of + button

--- RemoteAC_Android/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ RemoteAC_Android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -28,10 +28,8 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        binding.fab.setOnClickListener { 
+            navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
