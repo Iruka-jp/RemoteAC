17a48b9fa978215bdb9afeffb475902898086642
Iruka-jp <52767185+Iruka-jp@users.noreply.github.com>
2026-03-04 16:09:30 +0900

    ESP32 WiFi Captive Portal to retrieve credential

---
+++ hardware_esp32/CaptivePortal/CaptivePortal.ino
@@ -0,0 +1,36 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
+void setup() {
+    WiFi.mode(WIFI_STA); // Explicitly set mode to Station
+    Serial.begin(115200);
+
+    // Create an instance of WiFiManager
+    WiFiManager wm;
+
+    // Uncomment this line if you want to wipe saved settings for testing
+    // wm.resetSettings();
+
+    // This sets a timeout so the ESP32 doesn't stay in AP mode forever
+    // If no one connects in 180 seconds, it will move on/reboot
+    wm.setConfigPortalTimeout(180);
+
+    // autoConnect(AP_Name, AP_Password)
+    // This fetches SSID and pass from NVS and tries to connect.
+    // If it fails, it starts an AP with the specified name.
+    bool res;
+    res = wm.autoConnect("RemoteAC", "remote123"); 
+
+    if(!res) {
+        Serial.println("Failed to connect or hit timeout");
+        // ESP.restart();
+    } else {
+        // If you get here, you are connected to the WiFi!
+        Serial.println("Connected... yeey :)");
+        Serial.print("IP Address: ");
+        Serial.println(WiFi.localIP());
+    }
+}
+
+void loop() {
+    // Your normal code goes here
+}
